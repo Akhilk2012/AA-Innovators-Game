d410eafae808a3e9fadf9429c5614c1be14cdd95
Akhil <email@example.com>
2024-01-20 13:21:13 +0530

    Changed background color

--- Germ_invaders_Main.py
+++ Germ_invaders_Main.py
@@ -5,7 +5,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 640
 
-#Game variables
+# Game Variables
 
 icon = pygame.image.load('AA-Innovators-Game/Game Assets/Germs/icon.png')
 
@@ -17,10 +17,22 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Germ Invaders')
 pygame.display.set_icon(icon)
 
+
+# Colors
+
+BG = (69, 42, 29)
+
+def draw_bg():
+    screen.fill(BG)
+     
+
 # Game Loop
+     
 
 while run:
+    draw_bg()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
 pygame.quit()
