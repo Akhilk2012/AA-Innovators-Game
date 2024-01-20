1e4018989d6eab8ebfc20a2e53b40a0269522b63
Akhil <email@example.com>
2024-01-20 18:39:07 +0530

    Added animation to the player

--- Germ_invaders_Main.py
+++ Germ_invaders_Main.py
@@ -28,8 +28,14 @@ class Player(pygame.sprite.Sprite):
         self.speed = speed
         self.direction = 1
         self.flip = False
-        img = pygame.image.load('AA-Innovators-Game/Game Assets/Player/4.png')
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), (int(img.get_height() * scale))))
+        self.animation_list = []
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        for i in range(8):
+            img = pygame.image.load(f'AA-Innovators-Game/Game Assets/Player/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), (int(img.get_height() * scale))))
+            self.animation_list.append(img)
+        self.image = self.animation_list[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -47,13 +53,27 @@ class Player(pygame.sprite.Sprite):
 
         self.rect.x += dx 
         self.rect.y += dy 
+
+    def update_animation(self):
+        # Keep updating animation
+        ANIMATION_COOLDOWN = 100
+        # Update image depending on current frame
+        self.image = self.animation_list[self.frame_index]
+        # Check if time has passed since last update
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        # If the animation is done repeat
+        if self.frame_index >= len(self.animation_list):
+            self.frame_index = 0
+
                
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
-player = Player(200, 200, 1, 5)
- 
+player = Player(200, 200, 0.2, 5)
+  
 
 
 # Colors
@@ -71,6 +91,7 @@ while run:
     
     draw_bg()
     clock.tick(FPS)
+    player.update_animation()
     player.draw()
     player.motion(moving_left,moving_right)
 
